9c531c8ad7eca28a1ac424d3a7462fdc3b966b47
Paul Cameron <pcameron5@gmail.com>
2022-05-20 10:38:21 -0400

    CMOD-05202022-Header and we-are components created

---
+++ src/app/content/we-are/we-are.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeAreComponent } from './we-are.component';
+
+describe('WeAreComponent', () => {
+  let component: WeAreComponent;
+  let fixture: ComponentFixture<WeAreComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WeAreComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WeAreComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
